cfe4d0f3a2624babbd04828b20c284e435c7164d
alex <alex.titakoff@yandex.ru>
2017-12-05 21:10:00 +0300

    NewTask2

--- main.py
+++ main.py
@@ -6,3 +6,29 @@
 # Удачи.
 #
 # Напомню, если вы решаете новый модуль, то и загружать надо в новую ветку, пример: oappot/[Task]2
+
+filename = 'text.txt'
+
+
+def init(filename):
+    f = open('text.txt', 'r', encoding="utf-8")
+    list_words = f.read().split()
+
+    list_upper_words = []
+
+    for el in list_words:
+        if len(el) > 6 and el[6] != ',' and el[6] != '.':
+            list_upper_words.append(el.title())
+        else:
+            list_upper_words.append(el)
+
+    new_text = str(" ".join(list_upper_words))
+    print(new_text)
+
+    with open("text.txt", "w", encoding="utf-8") as f:
+        f.write(new_text)  # python will convert \n to os.linesep
+        f.close()
+
+
+if __name__ == '__main__':
+    init(filename)
