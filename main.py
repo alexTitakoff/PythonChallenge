85c86948222a79ad3acb765b8ba026277465948f
alex <alex.titakoff@yandex.ru>
2017-12-06 22:26:41 +0300

    New Task3

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+def decorator_func(inner_function):
+     def showing_n(n):
+         print(str(n))
+         inner_function(n)
+
+     return showing_n
+
+
+@decorator_func
 def untouchable_function(n):
-    if n == 0:
-        return
-    untouchable_function(n-1)
+     if n == 0:
+         return
+     untouchable_function(n - 1)
 
 
 if __name__ == '__main__':
